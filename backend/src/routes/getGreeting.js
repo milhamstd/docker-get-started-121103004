73be2d7144342e5bcfccabd1dd4358242a249c8a
milhamfzn <88817599+milhamfzn@users.noreply.github.com>
2024-11-03 06:12:55 +0700

    Add get-started files

---
+++ backend/src/routes/getGreeting.js
@@ -0,0 +1,10 @@
+const GREETING = [
+    "Whalecome!",
+    "All hands on the deck!",
+    "Charting the course ahead!"];
+
+module.exports = async (req, res) => {
+    res.send({
+        greeting: GREETING[Math.floor(Math.random() * GREETING.length)],
+    });
+};
